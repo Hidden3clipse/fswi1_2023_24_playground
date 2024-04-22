029b8f9a7c96be59156c0b6af2ad705dfaabbe19
Claus Gumbmann <claus.gumbmann@schule.bayern.de>
2024-04-22 19:00:23 +0200

    Klassen in Java - Einführungsbeispiel

--- src/main/java/Hauptanwendung.java
+++ src/main/java/Hauptanwendung.java
@@ -2,6 +2,16 @@ class Hauptanwendung {
 
     public static void main(String[] args) {
 
-        MeineMethoden.findAll();
+        Student student = new Student();
+        student.vorname = "Peter";
+        student.nachname = "Lustig";
+        student.geburtsdatum = "24.12.2000";
+        student.klasse = "FSWI-1";
+
+        System.out.println(String.format("%s,%s,%s,%s",
+            student.vorname,
+            student.nachname,
+            student.geburtsdatum,
+            student.klasse));
     }
 }
