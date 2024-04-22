3a67ef2e7276d2203325576d382136da210cff92
Claus Gumbmann <claus.gumbmann@schule.bayern.de>
2024-04-22 17:55:28 +0200

    Aufgaben zu den Methoden

--- src/main/java/Hauptanwendung.java
+++ src/main/java/Hauptanwendung.java
@@ -2,22 +2,22 @@ class Hauptanwendung {
 
     public static void main(String[] args) {
 
-        //A1
-        double [] numbers = {1.0,2.0,3.0,4.0,5.0};
+        String[][] studenten = new String[1][4]; 
+        studenten[0][0] = "Peter"; 
+        studenten[0][1] = "Lustig"; 
+        studenten[0][2] = "24.12.2001";     
+        studenten[0][3] = "FSWI-1";
 
-        //A2
-        String[] words = new String[3]; 
-        words[0] = "World";
-        words[1] = "Hello";
-        words[2] = " ";
-        System.out.printf("%s%s%s!", words[1], words[2], words[0]);
+        studenten = MeineMethoden.addOneEmptyItem(studenten);
+        studenten[1][0] = "Petra"; 
+        studenten[1][1] = "Lustig"; 
+        studenten[1][2] = "01.04.2000";     
+        studenten[1][3] = "FSWI-1";
 
-        //A3
-        String beforeCharArray = "JAVA ist cool";
-        char[] stringToCharArray = beforeCharArray.toLowerCase().toCharArray();
-        System.out.printf("%c", stringToCharArray[2]);
-
-        //A4
-        String[] arrayStudenten = {"Studet1","Student2","Student3","Student4","Student5"};
+        studenten = MeineMethoden.addOneEmptyItem(studenten);
+        studenten[2][0] = "Max"; 
+        studenten[2][1] = "Mustermann"; 
+        studenten[2][2] = "12.07.2001";     
+        studenten[2][3] = "FSWI-1";
     }
 }
