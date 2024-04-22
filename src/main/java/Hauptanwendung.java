cb10cec92ac1676ce8d2f8c68a7d8f4dba6469d6
Claus Gumbmann <claus.gumbmann@schule.bayern.de>
2024-04-22 17:34:59 +0200

    Aufgaben zu Arrays

--- src/main/java/Hauptanwendung.java
+++ src/main/java/Hauptanwendung.java
@@ -2,36 +2,22 @@ class Hauptanwendung {
 
     public static void main(String[] args) {
 
-        String firstName = "Peter";
-        String lastName = "Lustig";
+        //A1
+        double [] numbers = {1.0,2.0,3.0,4.0,5.0};
 
-        // V1
-        System.out.printf("%s, %s%n", lastName, firstName);
+        //A2
+        String[] words = new String[3]; 
+        words[0] = "World";
+        words[1] = "Hello";
+        words[2] = " ";
+        System.out.printf("%s%s%s!", words[1], words[2], words[0]);
 
-        //V2
-        System.out.printf("%s %s%n", firstName, lastName);
+        //A3
+        String beforeCharArray = "JAVA ist cool";
+        char[] stringToCharArray = beforeCharArray.toLowerCase().toCharArray();
+        System.out.printf("%c", stringToCharArray[2]);
 
-        //V3
-        System.out.printf("%s, %s%n", lastName.toUpperCase(), firstName);
-
-        //V4
-        System.out.printf("%s %s%n", firstName, lastName.toUpperCase());
-
-        //V5
-        System.out.printf("%s %s%n", firstName, lastName.toUpperCase().replace("", " "));
-
-        //V6
-        System.out.printf("%c. %s%n", firstName.toUpperCase().charAt(0), lastName);
-
-        //V7
-        System.out.printf("%c%c%n", firstName.toLowerCase().charAt(0), lastName.toLowerCase().charAt(0));
-
-        String fullName = firstName.concat(" ").concat(lastName);
-
-        fullName = fullName
-            .substring(fullName.indexOf(" ") + 1, fullName.length())
-            .concat(", ")
-            .concat(fullName.substring(0, fullName.indexOf(" ")));
-        System.out.println(fullName);
+        //A4
+        String[] arrayStudenten = {"Studet1","Student2","Student3","Student4","Student5"};
     }
 }
