4992266f998bf58a1f3b7436454b9820d58c8b0d
Claus Gumbmann <claus.gumbmann@schule.bayern.de>
2024-04-22 17:24:07 +0200

    Aufgaben zu den Zeichenfolgen

--- src/main/java/Hauptanwendung.java
+++ src/main/java/Hauptanwendung.java
@@ -2,5 +2,36 @@ class Hauptanwendung {
 
     public static void main(String[] args) {
 
+        String firstName = "Peter";
+        String lastName = "Lustig";
+
+        // V1
+        System.out.printf("%s, %s%n", lastName, firstName);
+
+        //V2
+        System.out.printf("%s %s%n", firstName, lastName);
+
+        //V3
+        System.out.printf("%s, %s%n", lastName.toUpperCase(), firstName);
+
+        //V4
+        System.out.printf("%s %s%n", firstName, lastName.toUpperCase());
+
+        //V5
+        System.out.printf("%s %s%n", firstName, lastName.toUpperCase().replace("", " "));
+
+        //V6
+        System.out.printf("%c. %s%n", firstName.toUpperCase().charAt(0), lastName);
+
+        //V7
+        System.out.printf("%c%c%n", firstName.toLowerCase().charAt(0), lastName.toLowerCase().charAt(0));
+
+        String fullName = firstName.concat(" ").concat(lastName);
+
+        fullName = fullName
+            .substring(fullName.indexOf(" ") + 1, fullName.length())
+            .concat(", ")
+            .concat(fullName.substring(0, fullName.indexOf(" ")));
+        System.out.println(fullName);
     }
 }
