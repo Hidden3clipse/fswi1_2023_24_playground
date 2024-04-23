525d5442236119e82730055d4ccce65cd58ea81b
Claus Gumbmann <claus.gumbmann@schule.bayern.de>
2024-04-23 09:50:07 +0200

    Umbau Klassen nach Java Standard

--- src/main/java/Hauptanwendung.java
+++ src/main/java/Hauptanwendung.java
@@ -1,17 +1,22 @@
+import models.Student;
+
 class Hauptanwendung {
 
     public static void main(String[] args) {
 
-        Student student = new Student();
-        student.vorname = "Peter";
-        student.nachname = "Lustig";
-        student.geburtsdatum = "24.12.2000";
-        student.klasse = "FSWI-1";
+        Student student = new Student(
+                "Peter",
+                "Lustig",
+                "24.12.2000",
+                "FSWI-1");
+
+        Student student2 = new Student("Lustig", "24.12.2000");
+        
 
         System.out.println(String.format("%s,%s,%s,%s",
-            student.vorname,
-            student.nachname,
-            student.geburtsdatum,
-            student.klasse));
+                student.getVorname(),
+                student.getNachname(),
+                student.getGeburtsdatum(),
+                student.getKlasse()));
     }
 }
---
+++ src/main/java/models/Student.java
@@ -0,0 +1,47 @@
+package models;
+public class Student {
+    private String vorname;
+	private String nachname;
+	private String geburtsdatum;
+	private String klasse;
+	
+	public Student(String vorname, String nachname, String geburtsdatum, String klasse) {
+		setVorname(vorname);
+		setNachname(nachname);
+		this.geburtsdatum = geburtsdatum;
+		setKlasse(klasse);
+    }
+
+	public Student(String nachname, String geburtsdatum) {
+		this.nachname = nachname;
+		this.geburtsdatum = geburtsdatum;
+    }
+
+	public String getVorname() {
+		return vorname;
+	}
+
+	public String getNachname() {
+		return nachname;
+	}
+
+	public String getGeburtsdatum() {
+		return geburtsdatum;
+	}
+
+	public String getKlasse() {
+		return klasse;
+	}
+
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
+	}
+
+	public void setNachname(String nachname) {
+		this.nachname = nachname;
+	}
+
+	public void setKlasse(String klasse) {
+		this.klasse = klasse;
+	}
+}
