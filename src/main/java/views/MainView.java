755fb5fa74c97ad692b140e724340d87cdf310e9
Claus Gumbmann <claus.gumbmann@schule.bayern.de>
2024-04-27 22:30:54 +0200

    Umbau MainView

--- src/main/java/views/MainView.java
+++ src/main/java/views/MainView.java
@@ -7,15 +7,19 @@ import javax.swing.JTable;
 import javax.swing.JScrollPane;
 import javax.swing.table.DefaultTableModel;
 
-public class MainView {
+public class MainView extends JFrame {
 
     private Student[] students = new Student[0];
 
     public MainView() {
+        // Aufruf des Konstruktor der Klasse JFrame
+        super("Übersicht Studenten");
 
+        // Methode loadDate, die die Studenten aus der CSV dem Array zuweist
         loadData();
-        JFrame frame = new JFrame("Übersicht Studenten");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // this = JFrame-Instanz; Verhalten, wenn das Fenster geschlossen wird
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // Create a JTable with students as content
         DefaultTableModel model = new DefaultTableModel();
@@ -28,15 +32,24 @@ public class MainView {
             model.addRow(new Object[]{student.getVorname(), student.getNachname(), student.getGeburtsdatum(), student.getKlasse()});
         }
 
+        // Insatnz der Klasse JTable = Tabellen-Ansicht
         JTable table = new JTable(model);
+
+        // Zum Scrollen, falls der Inhalt größer als die Ansicht ist
         JScrollPane scrollPane = new JScrollPane(table);
 
-        frame.add(scrollPane);
-        frame.pack();
-        frame.setVisible(true);
+        // ScrollPane zum Frame hinzufügen
+        this.add(scrollPane);
+
+        // Fenster zusammenpacken, dass die Größe stimmt
+        this.pack();
+
+        // Fenster sichtbar schalten
+        this.setVisible(true);
     }
 
     private void loadData() {
+        // Ruft die statische Methode findAll() der Klasse MeineMethoden auf 
         students =  MeineMethoden.findAll();
     }
 }
