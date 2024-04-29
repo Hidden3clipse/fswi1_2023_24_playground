1f7935afd7cda23bc0aecd613dd7cb69a715caa7
Claus Gumbmann <claus.gumbmann@schule.bayern.de>
2024-04-29 19:50:42 +0200

    Vorbereitung Unterricht 30.04.2024

--- src/main/java/Hauptanwendung.java
+++ src/main/java/Hauptanwendung.java
@@ -1,9 +1,6 @@
-import models.Student;
-import services.MeineMethoden;
-
 class Hauptanwendung {
 
     public static void main(String[] args) {
-       Student[] students =  MeineMethoden.findAll();
+       
     }
 }
